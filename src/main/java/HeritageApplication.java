8bb88cde2b69ac986630aeb0dce748aa6ae9de4e
Павловска Теодора <teodora.pavlovska@students.finki.ukim.mk>
2023-12-23 04:02:00 +0100

    main init commit

---
+++ src/main/java/HeritageApplication.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+
+public class HeritageApplication {
+    public static void main(String[] args) {
+
+            SpringApplication.run(HeritageApplication.class, args);
+
+    }
+}
