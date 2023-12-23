98bfeaced010bc29f3517733151572055b118485
Павловска Теодора <teodora.pavlovska@students.finki.ukim.mk>
2023-12-23 01:49:50 +0100

    added types into HeritageType

--- src/main/java/mk/ukim/finki/nationalheritage/model/HeritageType.java
+++ src/main/java/mk/ukim/finki/nationalheritage/model/HeritageType.java
@@ -1,5 +1,23 @@
 package mk.ukim.finki.nationalheritage.model;
 
 public enum HeritageType {
+    CASTLE,
+    ARCHEOLOGICAL_SITE,
+    MONUMENT,
+    TOWER,
+    MEMORIAL,
+    CHURCH,
+    AQUEDUCT,
+    MONASTERY,
+    yes,   // AS PROVIDED BY THE OPEN STREET MAP SOFTWARE
+    RUINS,
+    CITY_GATE,
+    BUILDING,
+    BATTLEFIELD,
+    TOMB,
+    BOUNDARY_STONE,
+    LOCOMOTIVE,
+    WAYSIDE_SHRINE,
+    AIRCRAFT
 
 }
