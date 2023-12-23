697cdcdfdb05465bbf8e644a7e38403faa6d5144
Павловска Теодора <teodora.pavlovska@students.finki.ukim.mk>
2023-12-23 14:55:54 +0100

    todos updated for the heritage repository

--- src/main/java/mk/ukim/finki/nationalheritage/repository/HeritageRepository.java
+++ src/main/java/mk/ukim/finki/nationalheritage/repository/HeritageRepository.java
@@ -1,5 +1,17 @@
 package mk.ukim.finki.nationalheritage.repository;
 
-public class HeritageRepository {
+import mk.ukim.finki.nationalheritage.model.Heritage;
+import mk.ukim.finki.nationalheritage.model.HeritageType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface HeritageRepository extends JpaRepository<Heritage,Long> {
+    //TODO add aditional filters
+    List<Heritage> filterByHeritageTypeAndLocation(HeritageType heritageType, String location);
+    List<Heritage> filterByHeritageType(HeritageType heritageType, String location);
+
+    List<Heritage> filterByLocation(HeritageType heritageType, String location);
 
 }
