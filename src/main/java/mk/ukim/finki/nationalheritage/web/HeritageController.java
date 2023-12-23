d5e6e45572d3f394a8370846bb262cd2ed85cef9
Павловска Теодора <teodora.pavlovska@students.finki.ukim.mk>
2023-12-23 04:14:30 +0100

    updated TODOS init commit for Controllers

---
+++ src/main/java/mk/ukim/finki/nationalheritage/web/HeritageController.java
@@ -0,0 +1,6 @@
+package mk.ukim.finki.nationalheritage.web;
+
+public class HeritageController {
+    //TODO
+
+}
---
+++ src/main/java/mk/ukim/finki/nationalheritage/web/UsersController.java
@@ -0,0 +1,5 @@
+package mk.ukim.finki.nationalheritage.web;
+
+public class UsersController {
+    //TODO
+}
