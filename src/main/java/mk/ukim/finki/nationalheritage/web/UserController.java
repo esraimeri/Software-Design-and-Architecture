049f223aa825657ffea72b759245ec1309127938
Павловска Теодора <teodora.pavlovska@students.finki.ukim.mk>
2023-12-23 15:56:10 +0100

    init controllers commit

---
+++ src/main/java/mk/ukim/finki/nationalheritage/web/UserController.java
@@ -0,0 +1,20 @@
+package mk.ukim.finki.nationalheritage.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+//TODO COMPLETE OCNTROLLERS
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/users")
+    public String showUserList() {
+        return "user/home.html";
+    }
+
+    @GetMapping("/login_register")
+    public String showUserForm() {
+        return "user/login_register.html";
+    }
+}
--- src/main/java/mk/ukim/finki/nationalheritage/web/UsersController.java
+++
@@ -1,5 +0,0 @@
-package mk.ukim.finki.nationalheritage.web;
-
-public class UsersController {
-    //TODO
-}
